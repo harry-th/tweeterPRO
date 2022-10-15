040871b59876a4f823d86d92db73c70d8f1f34f8
harry-th <42222781+harry-th@users.noreply.github.com>
2022-10-15 03:39:54 -0400

    boo

--- server/index.js
+++ server/index.js
@@ -100,10 +100,10 @@ const aws = require('aws-sdk');
 let s3 = new aws.S3({
   MONGO_URI: process.env.MONGO_URI,
 });
-
+console.log(s3.MONGO_URI)
 const start = async() => {
   try {
-    await databaseConnect(MONGO_URI);
+    await databaseConnect(s3.MONGO_URI);
     app.listen(process.env.PORT || PORT, () => {
       console.log("Example app listening on port " + PORT);
     });
